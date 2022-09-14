6dcd7aa78e617fdae7cf8d52b5197c90c79cad13
Vanaldito <fredimanuelb@gmail.com>
2022-09-14 08:06:29 -0600

    feat: add FilterDrawer component

--- src/components/Filter/index.tsx
+++ src/components/Filter/index.tsx
@@ -1,19 +1,49 @@
+import { useState } from "react";
+import FilterDrawer from "../FilterDrawer";
 import SearchIcon from "../Icons/Search";
 
 import "./styles.css";
 
 export default function Filter() {
+  const [fieldToModify, setFieldToModify] = useState<
+    null | "guests" | "location"
+  >(null);
+  const [location, setLocation] = useState("Helsinki, Findland");
+  const [guests, setGuests] = useState(0);
+
+  function modifyField(fieldName: "guests" | "location") {
+    return () => {
+      setFieldToModify(fieldName);
+    };
+  }
+
+  function clearFieldToModify() {
+    setFieldToModify(null);
+  }
+
   return (
     <div className="filter">
       <div className="filter__form">
-        <div className="filter__city">Helsinki, Finland</div>
-        <button className="filter__guests" type="button">
+        <div className="filter__city" onClick={modifyField("location")}>
+          {location}
+        </div>
+        <button
+          className="filter__guests"
+          type="button"
+          onClick={modifyField("guests")}
+        >
           Add guests
         </button>
         <button className="filter__search-button" type="submit">
           <SearchIcon />
         </button>
       </div>
+      {fieldToModify && (
+        <FilterDrawer
+          defaultFieldToModify={fieldToModify}
+          closeDrawer={clearFieldToModify}
+        />
+      )}
     </div>
   );
 }
---
+++ src/components/FilterDrawer/index.tsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+
+import "./styles.css";
+
+interface FilterDrawerProps {
+  defaultFieldToModify: "location" | "guests";
+  closeDrawer: () => void;
+}
+
+export default function FilterDrawer({
+  defaultFieldToModify,
+  closeDrawer,
+}: FilterDrawerProps) {
+  const [fieldToModify, setFieldToModify] = useState(defaultFieldToModify);
+
+  return (
+    <div className="filter-drawer">
+      <header className="filter-drawer__header">
+        <div>Edit you search</div>
+        <button
+          className="filter-drawer__close-button"
+          type="button"
+          onClick={closeDrawer}
+        >
+          Close
+        </button>
+      </header>
+      <div className="filter-drawer__fields">
+        <div
+          className="filter-drawer__field"
+          onClick={() => setFieldToModify("location")}
+        >
+          <div className="filter-drawer__field__title">Location</div>
+          <div className="filter-drawer__field__value">Helsinki, Finland</div>
+        </div>
+        <div
+          className="filter-drawer__field"
+          onClick={() => setFieldToModify("guests")}
+        >
+          <div className="filter-drawer__field__title">Guests</div>
+          <div className="filter-drawer__field__placeholder">Add guests</div>
+        </div>
+      </div>
+    </div>
+  );
+}
