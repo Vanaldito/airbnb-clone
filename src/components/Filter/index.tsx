65175d4d001bb18199c285cc355661c3b7e17872
Vanaldito <fredimanuelb@gmail.com>
2022-09-13 20:48:43 -0600

    fix: change the filter form to div and input to button

--- src/components/Filter/index.tsx
+++ src/components/Filter/index.tsx
@@ -5,13 +5,15 @@ import "./styles.css";
 export default function Filter() {
   return (
     <div className="filter">
-      <form className="filter__form">
+      <div className="filter__form">
         <div className="filter__city">Helsinki, Finland</div>
-        <input className="filter__guests" placeholder="Add guests" />
+        <button className="filter__guests" type="button">
+          Add guests
+        </button>
         <button className="filter__search-button" type="submit">
           <SearchIcon />
         </button>
-      </form>
+      </div>
     </div>
   );
 }
